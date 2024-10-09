0c087b1a9eb3516fd272d710a93cffab8a568a00
xnought <dbertucci6@gatech.edu>
2024-10-09 14:20:49 -0400

    by default change to localhost

--- viewer/website/src/backend.ts
+++ viewer/website/src/backend.ts
@@ -3,5 +3,5 @@ export { DefaultService as Backend } from "./openapi";
 import { OpenAPI } from "./openapi";
 
 // sets url for all Backend.func() calls
-// OpenAPI.BASE = "http://localhost:4322"; // for deployment
-OpenAPI.BASE = "https://ocular.cc.gatech.edu/DS569k-backend/";
+OpenAPI.BASE = "http://localhost:4322";
+// OpenAPI.BASE = "https://ocular.cc.gatech.edu/DS569k-backend/"; // for deployment
